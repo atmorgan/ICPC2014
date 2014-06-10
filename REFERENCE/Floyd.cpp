00efb14638d71a75f85eac6327ad72d156391381
Andrew Morgan <atmorgan@wisc.edu>
2014-06-10 17:09:56 -0500

    Update floyd-warshall

--- REFERENCE/Floyd.cpp
+++ REFERENCE/Floyd.cpp
@@ -1,5 +1,5 @@
 // This function runs the Floyd-Warshall algorithm for all-pairs
-// shortest paths.  Also handles negative edge weights.  Returns true
+// shortest paths.	Also handles negative edge weights.	Returns true
 // if a negative weight cycle is found.
 //
 // Running time: O(|V|^3)
@@ -18,23 +18,20 @@ typedef vector<TYPE> VT;
 typedef vector<VT> VVT;
 typedef vector<int> VI;
 typedef vector<VI> VVI;
-bool FloydWarshall (VVT &w, VVI &prev){
-  int n = w.size();
-  prev = VVI (n, VI(n, -1));
-  
-  for (int k = 0; k < n; k++){
-    for (int i = 0; i < n; i++){
-      for (int j = 0; j < n; j++){
-      	if (w[i][j] > w[i][k] + w[k][j]){
-      	  w[i][j] = w[i][k] + w[k][j];
-      	  prev[i][j] = k;
-      	}
-      }
-    }
-  }
-  // check for negative weight cycles
-  for (int i = 0; i < n; i++)
-    if (w[i][i] < 0) return false;
-  return true;    
+bool FloydWarshall (VVT &w, VVI &prev) {
+	int n = w.size();
+	prev = VVI (n, VI(n, -1));
+	for( int k = 0; k < n; k++ )
+	for( int i = 0; i < n; i++ )
+	for( int j = 0; j < n; j++ ) {
+		if( w[i][j] > w[i][k] + w[k][j] ) {
+			w[i][j] = w[i][k] + w[k][j];
+			prev[i][j] = k;
+		}
+	}
+	// check for negative weight cycles
+	for (int i = 0; i < n; i++)
+		if (w[i][i] < 0) return false;
+	return true;		
 }
 // END
