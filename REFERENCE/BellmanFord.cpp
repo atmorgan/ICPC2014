6b9e493af302052b31cb3b1ef9b91682d52e333e
Andrew Morgan <atmorgan@wisc.edu>
2014-06-10 17:06:18 -0500

    fix up bellman-ford

--- REFERENCE/BellmanFord.cpp
+++ REFERENCE/BellmanFord.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 // BEGIN
 // This function runs the Bellman-Ford algorithm for single source
-// shortest paths with negative edge weights.  The function returns
-// false if a negative weight cycle is detected.  Otherwise, the
+// shortest paths with negative edge weights.	The function returns
+// false if a negative weight cycle is detected.	Otherwise, the
 // function returns true and dist[i] is the length of the shortest
 // path from start to i.
 //
@@ -15,30 +15,28 @@ using namespace std;
 //   INPUT:   start, w[i][j] = cost of edge from i to j
 //   OUTPUT:  dist[i] = min weight path from start to i
 //            prev[i] = previous node on the best path from the
-//                      start node   
+//                      start node
 typedef int TYPE;
 typedef vector<TYPE> VT;
 typedef vector<VT> VVT;
 typedef vector<int> VI;
 typedef vector<VI> VVI;
 bool BellmanFord (const VVT &w, VT &dist, VI &prev, int start){
-  int n = w.size();
-  prev = VI(n, -1);
-  dist = VT(n, 1000000000);
-  dist[start] = 0;
-
-  for (int k = 0; k < n; k++){
-    for (int i = 0; i < n; i++){
-      for (int j = 0; j < n; j++){
-	if (dist[j] > dist[i] + w[i][j]){
-	  if (k == n-1) return false;
-	  dist[j] = dist[i] + w[i][j];
-	  prev[j] = i;
-	}	  
-      }
-    }
-  }
-
-  return true;
+	int n = w.size();
+	prev = VI(n, -1);
+	dist = VT(n, 1000000000);
+	dist[start] = 0;
+	for (int k = 0; k < n; k++){
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < n; j++){
+				if (dist[j] > dist[i] + w[i][j]){
+					if (k == n-1) return false;
+					dist[j] = dist[i] + w[i][j];
+					prev[j] = i;
+				}
+			}
+		}
+	}
+	return true;
 }
 // END
