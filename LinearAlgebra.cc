35ae4d0016dc75b6f3a681ee75d7b07c946f9672
Andrew Morgan <atmorgan@wisc.edu>
2014-06-18 12:39:05 -0500

    semi-update LinearAlgebra.cc (no guarantee of compiling and certainly no testing)

--- LinearAlgebra.cc
+++ LinearAlgebra.cc
@@ -1,63 +1,85 @@
-#include <iostream>
 #include <vector>
 #include <cmath>
-
 using namespace std;
 // BEGIN
-// Gauss-Jordan elimination with full pivoting.
-//
-// Uses:
-//   (1) solving systems of linear equations (AX=B)
-//   (2) inverting matrices (AX=I)
-//   (3) computing determinants of square matrices
-//
-// Running time: O(n^3)
-//
-// INPUT:    a[][] = an nxn matrix
-//           b[][] = an nxm matrix
-//
-// OUTPUT:   X      = an nxm matrix (stored in b[][])
-//           A^{-1} = an nxn matrix (stored in a[][])
-//           returns determinant of a[][]
-const double EPS = 1e-10;
-typedef vector<int> VI;
-typedef double T;
-typedef vector<T> VT;
-typedef vector<VT> VVT;
-T GaussJordan(VVT &a, VVT &b) {
-	const int n = a.size();
-	const int m = b[0].size();
-	VI irow(n), icol(n), ipiv(n);
+// Useful linear algebra routines.
+#define FOR(v,l,u) for( size_t v = l; v < u; ++v )
+typedef double         T;   // the code below only supports fields
+typedef vector<T>      VT;
+typedef vector<VT>     VVT;
+typedef vector<size_t> VI;
+const T EPS = 1e-10;
+// Given an m-by-n matrix A, compute its reduced row echelon form,
+// returning a value like the determinant.
+// If m = n, the returned value *is* the determinant of A.
+// If m != n, the returned value is nonzero iff A has full row rank.
+// To compute rank(A), get its RREF, and count the nonzero rows.
+T GaussJordan( VVT &A ) {
+	const size_t m = A.size(), n = A[0].size();
 	T det = 1;
-	for (int i = 0; i < n; i++) {
-		int pj = -1, pk = -1;
-		for (int j = 0; j < n; j++) if (!ipiv[j])
-			for (int k = 0; k < n; k++) if (!ipiv[k])
-	if (pj == -1 || fabs(a[j][k]) > fabs(a[pj][pk])) { pj = j; pk = k; }
-		if (fabs(a[pj][pk]) < EPS) { cerr << "Matrix is singular." << endl; exit(0); }
-		ipiv[pk]++;
-		swap(a[pj], a[pk]);
-		swap(b[pj], b[pk]);
-		if (pj != pk) det *= -1;
-		irow[i] = pj;
-		icol[i] = pk;
-		T c = 1.0 / a[pk][pk];
-		det *= a[pk][pk];
-		a[pk][pk] = 1.0;
-		for (int p = 0; p < n; p++) a[pk][p] *= c;
-		for (int p = 0; p < m; p++) b[pk][p] *= c;
-		for (int p = 0; p < n; p++) if (p != pk) {
-			c = a[p][pk];
-			a[p][pk] = 0;
-			for (int q = 0; q < n; q++) a[p][q] -= a[pk][q] * c;
-			for (int q = 0; q < m; q++) b[p][q] -= b[pk][q] * c;			
+	size_t p = 0;         // walking pointer for the pivot column
+	FOR(k,0,m) {          // for each row...
+		while( p < n ) {    // find the best row below k to pull up
+			size_t i_max = k; // index of the best row
+			FOR(i,k,m) if( fabs(A[i][p]) > fabs(A[i_max][p]) ) i_max = i;
+			if( fabs(A[i_max][p]) > EPS ) {  // We have our pivot row
+				if( i_max != k ) {             // swap if it's not k
+					swap( A[i_max], A[k] );
+					det *= -1;
+				}
+				break;                         // don't let p grow to n
+			}
+			FOR(i,k,m) A[i][p] = 0; // This column is zeros below row k
+			++p;                    // So move on to the next column
 		}
-	}
-	for (int p = n-1; p >= 0; p--) if (irow[p] != icol[p]) {
-		for (int k = 0; k < n; k++) swap(a[k][irow[p]], a[k][icol[p]]);
+		if( p == n ) { det = 0; break; } // we're done early
+		T s = 1/A[k][p];         // scale the pivot row
+		FOR(j,p,n) A[k][j] *= s;
+		det *= s;
+		// subtract pivot row from the other rows
+		FOR(i,0,m) if( i != k ) FOR(j,p,n) A[i][j] -= A[i][p]*A[k][j];
+		++p;
 	}
 	return det;
 }
+// In-place invert A.
+void InvertMatrix( VVT &A ) {
+	const size_t n = A.size();
+	FOR(i,0,n) FOR(j,0,n) A[i].push_back( (i==j) ? 1 : 0 ); // augment
+	GaussJordan( A );                          // compute RREF
+	FOR(i,0,n) FOR(j,0,n) A[i][j] = A[i][j+n]; // copy A inverse over
+	FOR(i,0,n) A[i].resize(n);                 // get rid of cruft
+}
+// Given n-by-n A and n-by-q b, compute a vector x with Ax = b.
+// The output is returned in x, when it exists. (else, x is empty)
+// If there are solutions, return the dimension of the kernel of A
+// (which equals the dimension of the affine space of solutions),
+// or else return -1 when there are no solutions.
+// (There is a unique solution iff we return 0.)
+int SolveLinearSystem( const VVT &A, const VVT &b, VVT &x ) {
+	const size_t n = A.size(), q = b[0].size();
+	x = VVT(A);                                    // copy
+	FOR(i,0,n) FOR(j,0,q) x[i].push_back(b[i][j]); // augment
+	GaussJordan( x );                              // RREF
+	int kerd = 0;
+	FOR(i,0,n) { // dim(ker(A)) = # of all-zero rows
+		FOR(j,0,n) if( fabs(x[i][j]) > EPS )   goto solexists;
+		FOR(j,0,q) if( fabs(x[i][n+j]) > EPS ) goto nosolution;
+		++kerd;
+	}
+	solexists: {
+		FOR(i,0,n) FOR(j,0,q) x[i][j] = x[i][n+j];
+		FOR(i,0,n) x[i].resize(q);
+		return kerd;
+	}
+	nosolution: {
+		x.clear();
+		return -1;
+	}
+}
+// END
+
+/*  Old main-- useful for testing
 int main() {
 	const int n = 4;
 	const int m = 2;
@@ -92,4 +114,4 @@ int main() {
 		cout << endl;
 	}
 }
-// END
+*/
