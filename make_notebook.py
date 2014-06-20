e3b7da1eebb657abfc535739e84ca668682d228e
Andrew Morgan <atmorgan@wisc.edu>
2014-06-20 10:44:47 -0500

    update notebook-making script

--- make_notebook.py
+++ make_notebook.py
@@ -14,6 +14,8 @@ _sections = {
 		("Bellman-Ford", 'cpp', "BellmanFord.cc"),
 		("Floyd-Warshall", 'cpp', "FloydWarshall.cc"),
 		("Maximum Flow (Dinic)", 'cpp', "MaximumFlowDinic.cc"),
+		("Maximum Flow (Push-relabel)", 'cpp', "MaximumFlow_PushRelabel.cc"),
+		("Minimum-cost Maximum Flow", 'cpp', "MinCostMaxFlow.cc"),
 		("Strongly-connected Components", 'cpp', "SCC.cc"),
 	],
 	"Maths": [
@@ -22,20 +24,21 @@ _sections = {
 		("Simplex", 'cpp', "Simplex.cc"),
 	],
 	"Geometry": [
+		("Basic floating point comparison", 'cpp', "FloatCompare.cc"),
 		("Basic Vector/Point", 'cpp', "Vector.cc"),
 		("Plane Geometry", 'cpp', "PlaneGeometry.cc"),
 		("Polygon", 'cpp', "Polygon.cc"),
 	],
 	"Strings": [
 		("Knuth-Morris-Pratt Table/Algorithm", 'cpp', "KMP.cc"),
-		("Suffix Arrays", 'cpp', "SuffixArray.cc"),
+		("Suffix Array/LCP", 'cpp', "SuffixArray.cc"),
 	]
 }
 
 _fmtnames = {'cpp' : 'C++', 'java' : 'Java'}
 
 _re_START  = re.compile('\s*//\s*BEGIN\s*$')
-_re_END	= re.compile('\s*//\s*END\s*$')
+_re_END    = re.compile('\s*//\s*END\s*$')
 
 def preprocess(fn):
 	out = ""
@@ -107,6 +110,6 @@ pre { font-size: 11pt }
 	print( '</BODY></HTML>' )
 
 
-if __name__ == "__main__":			
+if __name__ == "__main__":
 	make_notebook(_sections)
 
