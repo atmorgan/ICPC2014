eb7cc71f4b4a9fbac3434a03219b46911c336a89
Andrew Morgan <amorgan@cs.wisc.edu>
2015-04-11 11:45:57 -0500

    rework dinic implementation to be easier to code/check. still needs tests!

--- MaximumFlow-Dinic.cc
+++ MaximumFlow-Dinic.cc
@@ -1,10 +1,84 @@
-#include <cstring>
-#include <cstdio>
 #include <algorithm>
+#include <limits>
+#include <vector>
 
 using namespace std;
 
+#define FOR(v,l,u) for( size_t v = l; v < u; ++v )
+
 // BEGIN
+typedef double T; // also works for doubles, but use feq, etc
+const T INFTY = numeric_limits<T>::max(); // ::infinity() for doubles
+
+typedef vector<size_t> VI;
+typedef vector<VI>     VVI;
+typedef vector<T>      VT;
+typedef vector<VT>     VVT;
+
+struct dinic_graph {
+	size_t N;
+	VVI A;
+	VVT C, F;  // references to F can be removed if you don't want flows
+	dinic_graph( size_t _N ) : N(_N), A(N), C(N,VT(N,0)), F(N,VT(N,0)) {}
+	void add_capacity( size_t s, size_t t, T cap ) {
+		if( cap == 0 ) return;
+		if( C[s][t] == 0 && C[t][s] == 0 ) {
+			A[s].push_back(t);
+			A[t].push_back(s);
+		}
+		C[s][t] += cap;
+		// If you subtract capacities, and want to remove edges with cap 0,
+		// do so here, or afterward.
+	}
+	T Augment( const VI &P ) {
+		T amt = INFTY;
+		FOR(i,0,P.size()-1) amt = min(amt, C[ P[i] ][ P[i+1] ]);
+		FOR(i,0,P.size()-1) {
+			size_t u = P[i], v = P[i+1];
+			C[u][v] -= amt;
+			C[v][u] += amt;
+			if( F[v][u] > amt ) {
+				F[v][u] -= amt;
+			}
+			else {
+				F[u][v] += amt - F[v][u];
+				F[v][u] = 0;
+			}
+		}
+		return amt;
+	}
+	bool bfs( size_t s, size_t t, VI &P ) {
+		P = VI(N,N);
+		VI Q(N);  size_t qh=0, qt=0;
+		P[  Q[qt++] = s  ] = s;
+		while( qh < qt && P[t] == N ) {
+			size_t c = Q[qh++];
+			FOR(i,0,A[c].size()) {
+				size_t u = A[c][i];
+				if( C[c][u] == 0 ) continue;
+				if( P[u] != N ) continue;
+				P[  Q[qt++] = u  ] = c;
+			}
+		}
+		return P[t] != N;
+	}
+	T ComputeMaxFlow( size_t s, size_t t ) {
+		T flow = 0;
+		VI P;
+		while( bfs(s,t,P) ) {
+			VI path(1,t);
+			size_t z = t;
+			while( z != P[z] ) path.push_back( z = P[z] );
+			path = VI(path.rbegin(), path.rend());
+			flow += Augment(path);
+		}
+		return flow;
+	}
+};
+// END
+
+// TODO: TESTS!
+
 // Ripped from http://web.stanford.edu/~liszt90/acm/notebook.html#file1
 /****************
  * Maximum flow * (Dinic's on an adjacency list + matrix)
@@ -25,6 +99,8 @@ using namespace std;
  * RUNNING TIME:
  *      - O(n^3)
  **/
+#include <cstring>
+#include <cstdio>
 
 // the maximum number of vertices
 #define NN 1024
@@ -85,4 +161,3 @@ int main() {
     printf( "%d\n", dinic( n, s, t ) );
     return 0;
 }
-// END
