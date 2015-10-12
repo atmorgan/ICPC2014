664efa754f594374d25dde0745a83c0ac659a75d
Bryce Sandlund <bcsandlund@gmail.com>
2015-10-12 16:36:28 -0500

    Max Flow Updates
    -Made MCBM use adj lists, making it faster
    -Removed extraneous ripped stanford code from Dinic and EdmondsKarp

--- MaxCardBipartiteMatching.cc
+++ MaxCardBipartiteMatching.cc
@@ -8,22 +8,26 @@ using namespace std;
 //
 // Running time: O(|E| |V|) -- often much faster in practice
 //
-//   INPUT: w[i][j] = edge between row node i and column node j
+//   INPUT: adj_list[i][j] = edge between row node i and column node adj_list[i][j]
+//          mr[i] = vector of size #rows, initialized to -1
+//          mc[j] = vector of size #columns, initialized to -1
+//          
 //   OUTPUT: mr[i] = assignment for row node i, -1 if unassigned
 //           mc[j] = assignment for column node j, -1 if unassigned
 //           function returns number of matches made
 
 typedef vector<int> VI;
+typedef vector<VI> VVI;
 typedef vector<bool> VB;
-typedef vector<VB> VVB;
 
-bool FindMatch(int i, const VVB &w, VI &mr, VI &mc, VB &seen) {
-  for (int j = 0; j < w[i].size(); j++) {
-    if (w[i][j] && !seen[j]) {
-      seen[j] = true;
-      if (mc[j] < 0 || FindMatch(mc[j], w, mr, mc, seen)) {
-        mr[i] = j;
-        mc[j] = i;
+bool FindMatch(int i, const VVI &adj_list, VI &mr, VI &mc, VB &seen) {
+  for (int j = 0; j < adj_list[i].size(); j++) {
+    int item = adj_list[i][j];
+    if (!seen[item]) {
+      seen[item] = true;
+      if (mc[item] < 0 || FindMatch(mc[item], adj_list, mr, mc, seen)) {
+        mr[i] = item;
+        mc[item] = i;
         return true;
       }
     }
@@ -31,14 +35,13 @@ bool FindMatch(int i, const VVB &w, VI &mr, VI &mc, VB &seen) {
   return false;
 }
 
-int BipartiteMatching(const VVB &w, VI &mr, VI &mc) {
-  mr = VI(w.size(), -1);
-  mc = VI(w[0].size(), -1);
-  
+// mr should be a vector of size number of row items, initialized to -1
+// mc should be a vector of size number of column items, initialized to -1
+int BipartiteMatching(const VVI &adj_list, VI &mr, VI &mc) {
   int ct = 0;
-  for (int i = 0; i < w.size(); i++) {
-    VB seen(w[0].size(), false);
-    if (FindMatch(i, w, mr, mc, seen)) ct++;
+  for (int i = 0; i < adj_list.size(); i++) {
+    VB seen(mc.size(), false);
+    if (FindMatch(i, adj_list, mr, mc, seen)) ct++;
   }
   return ct;
 }
