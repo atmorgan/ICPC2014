9d6d0fee20d7bf2336f93172e7c11179caca0601
Bryce Sandlund <bcsandlund@gmail.com>
2015-10-20 14:21:02 -0500

    SegmentTree and BIT Updates
    - Made segment tree static. This is faster, necessary for AhoyPirates.
    Also added a few other tricks that give good speed.
    - Made BIT inclusive on both ends for all queries, and updated test
    code respectively.

--- BIT.cc
+++ BIT.cc
@@ -1,8 +1,9 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-#define FOR(v,l,u) for( size_t v = l; v < u; ++v )
+#define FOR(v,l,u) for( int v = l; v < u; ++v )
 
 typedef signed long long int LL;
 typedef LL T;
@@ -12,7 +13,7 @@ typedef vector<T> VT;
 // T is a type with +/- operations and identity element '0'.
 
 // Least significant bit of a. Used throughout.
-size_t LSB( size_t a ) { return a ^ (a & (a-1)); }
+int LSB( int a ) { return a ^ (a & (a-1)); }
 
 // To use it, instantiate it as `BIT(n)' where n is the size of the underlying
 // array. The BIT then assumes a value of 0 for every element. Update each
@@ -20,101 +21,104 @@ size_t LSB( size_t a ) { return a ^ (a & (a-1)); }
 //
 // Note that it is assumed that the underlying array has size a power of 2!
 // This mostly just simplifies the implementation without any loss in speed.
+// Just use the closest power of 2 larger than the max input size. Even if some test
+// cases do not test this high, initialization is extremely quick.
 //
 // The comments below make reference to an array `arry'. This is the underlying
 // array. (A is the data stored in the actual tree.)
 struct BIT {
-	size_t N;
+	int N;
 	VT A;
-	BIT( size_t n ) : N(n), A(N+1,0) {}
+	BIT( int n ) : N(n), A(N+1,0) {} // n must be a power of 2
 	// add v to arry[idx]
-	void add( size_t idx, T v ) {
-		for( size_t i = idx+1; i <= N; i += LSB(i) ) A[i] += v;
+	void add( int idx, T v ) {
+		for( int i = idx+1; i <= N; i += LSB(i) ) A[i] += v;
 	}
-	// get sum( arry[0..idx) )
-	T sum( size_t idx ) {
+	// get sum( arry[0..idx] )
+	T sum( int idx ) {
 		T ret = 0;
-		for( size_t i = idx; i > 0; i -= LSB(i) ) ret += A[i];
+		for( int i = idx+1; i > 0; i -= LSB(i) ) ret += A[i];
 		return ret;
 	}
-	// get sum( arry[l..r) )
-	T sum_range( size_t l, size_t r ) { return sum(r) - sum(l); }
-	// Find largest r so that sum( arry[0..r) ) <= thresh
+	// get sum( arry[l..r] )
+	T sum_range( int l, int r ) { return sum(r) - sum(l-1); }
+	// Find largest r so that sum( arry[0..r] ) <= thresh
 	// This assumes arry[i] >= 0 for all i > 0, for monotonicity.
 	// This takes advantage of the specific structure of LSB() to simplify the
 	// binary search.
-	size_t largest_at_most( T thresh ) {
-		size_t r = 0, del = N;
+	int largest_at_most( T thresh ) {
+		int r = 0, del = N;
 		while( del && r <= N ) {
-			size_t q = r + del;
+			int q = r + del;
 			if( A[q] <= thresh ) {
 				r = q;
 				thresh -= A[q];
 			}
 			del /= 2;
 		}
-		return r;
+		return r-1;
 	}
 };
 
 // A `range-add'/`index query' BIT
 struct BIT_flip {
 	BIT A;
-	BIT_flip( size_t n ) : A(n) {}
-	// add v to arry[l,r)
-	void add( size_t l, size_t r, T v ) {
+	BIT_flip( int n ) : A(n) {}
+	// add v to arry[l,r]
+	void add( int l, int r, T v ) {
 		A.add(l,v);
-		A.add(r,-v);
+		A.add(r+1,-v);
 	}
 	// get arry[idx]
-	T query( size_t idx ) {
-		return A.sum(idx+1);
+	T query( int idx ) {
+		return A.sum(idx);
 	}
 };
 
 // A `range-add'/`range-query' data structure that uses BITs.
 struct BIT_super {
-	size_t N;
+	int N;
 	BIT_flip m, b; // linear coefficient, constant coefficient
-	BIT_super( size_t n ) : N(n), m(n), b(n) {}
-	// add v to arry[l..r)
-	void add( size_t l, size_t r, T v ) {
-		m.add(l,r,v);         // add slope on active interval
+	BIT_super( int n ) : N(n), m(n), b(n) {}
+	// add v to arry[l..r]
+	void add( int l, int r, T v ) {
+        m.add(l,r,v);         // add slope on active interval
 		b.add(l,N,l*(-v));    // subtract contribution from pre-interval
-		b.add(r,N,r*v);       // add total contribution to after-interval
+		b.add(r+1,N,(r+1)*v);       // add total contribution to after-interval
 	}
-	// get sum( arry[0..r) )
-	T query( size_t r ) {
-		return m.query(r)*r + b.query(r);
+	// get sum( arry[0..r] )
+	T query( int r ) {
+		++r;
+        return m.query(r)*r + b.query(r);
 	}
-	// get sum( arry[l..r) )
-	T query_range( size_t l, size_t r ) {
-		return query(r) - query(l);
+	// get sum( arry[l..r] )
+	T query_range( int l, int r ) {
+		return query(r) - query(l-1);
 	}
 };
 
 // A 2-dimensional specialization of BITd. (see below)
 // What took `nlogn' before now takes `nlog^2(n)'.
 struct BIT2 {
-	size_t N1;
-	size_t N2;
+	int N1;
+	int N2;
 	vector<BIT> A;
-	BIT2( size_t n1, size_t n2 ) : N1(n1), N2(n2) {
-		FOR(i,0,N1) A.push_back( BIT(n2) );
+	BIT2( int n1, int n2 ) : N1(n1), N2(n2) {
+		A.resize(N1+1, BIT(n2));
 	}
 	// add v to arry[x][y]
-	void add( size_t x, size_t y, T v ) {
-		for( size_t i = x+1; i <= N1; i += LSB(i) ) A[i].add(y,v);
-	}
-	// get sum( arry[0..x)[0..y) ).
-	T sum( size_t x, size_t y ) {
-		T ret = 0;
-		for( size_t i = x; i > 0; i -= LSB(i) ) ret += A[i].sum(y);
+	void add( int x, int y, T v ) {
+        for( int i = x+1; i <= N1; i += LSB(i) ) A[i].add(y,v);
+    }
+	// get sum( arry[0..x][0..y] ).
+	T sum( int x, int y ) {
+        T ret = 0;
+		for( int i = x+1; i > 0; i -= LSB(i) ) ret += A[i].sum(y);
 		return ret;
 	}
 	// get sum( arry[xL..xH)[yL..yH) ).
-	T sum_range( size_t xL, size_t yL, size_t xH, size_t yH ) {
-		return sum(xH,yH) + sum(xL,yL) - sum(xH,yL) - sum(xL,yH);
+	T sum_range( int xL, int yL, int xH, int yH ) {
+		return sum(xH,yH) + sum(xL-1,yL-1) - sum(xH,yL-1) - sum(xL-1,yH);
 	}
 };
 
@@ -123,35 +127,36 @@ struct BIT2 {
 //
 // To construct it, set dims to be the vector of dimensions, and pass
 // d <- dims.size().
-typedef vector<size_t> VI;
+typedef vector<int> VI;
 struct BITd {
-	size_t N;
-	size_t D;
+	int N;
+	int D;
 	vector<BITd> A;
 	T V;
-	BITd( const VI &dims, size_t d ) : N(dims[d-1]), D(d) {
+	BITd( const VI &dims, int d ) : N(dims[d-1]), D(d) {
 		if( D == 0 ) V = 0;
-		else         A = vector<BITd>( N+1, BITd( dims, D-1 ) );
+		else         A.resize( N+1, BITd( dims, D-1 ) );
 	}
 	void add( const VI &idx, T v ) {
 		if( D == 0 ) V += v;
-		for( size_t i = idx[D-1]+1; i <= N; i += LSB(i) ) A[i].add(idx,v);
+		for( int i = idx[D-1]+1; i <= N; i += LSB(i) ) A[i].add(idx,v);
 	}
 	T sum( const VI &idx ) {
 		if( D == 0 ) return V;
 		T ret = 0;
-		for( size_t i = idx[D-1]; i > 0; i -= LSB(i) ) ret += A[i].sum(idx);
+		for( int i = idx[D-1]+1; i > 0; i -= LSB(i) ) ret += A[i].sum(idx);
 		return ret;
 	}
-	T sum_range( const VI &up, const VI &dn ) {
-		// In higher dimensions, we have to use inclusion-exclusion
-		size_t BD = ((size_t)1) << D;
+	T sum_range( VI lo, VI hi ) {
+		FOR(i,0,D) --lo[i];
+        // In higher dimensions, we have to use inclusion-exclusion
+		int BD = ((int)1) << D;
 		T ret = 0;
 		FOR(S,0,BD) {
 			int sign = 1;
-			VI q(up);
+			VI q(lo);
 			FOR(b,0,BD) if( (S >> b) & 1 ) {
-				q[b] = dn[b];
+				q[b] = hi[b];
 				sign *= -1;
 			}
 			ret += sign * sum(q);
@@ -168,31 +173,31 @@ void test_BIT_correct() {
     bool success = true;
     // only two test suites...
 	{
-		const size_t N  = (1U << 10); // ~1e3
-		const size_t NN = N*N;        // ~1e6
+		const int N  = (1U << 10); // ~1e3
+		const int NN = N*N;        // ~1e6
 		BIT tr(NN); // BIT on NN elts
 		// Basic tests
 		FOR(i,0,NN) tr.add( i, ((T)56)*i*(i+3) );
 		VT A(NN+1); // prefix sums, for checking
 		A[0] = 0; FOR(i,1,tr.N+1) A[i] = A[i-1] + ((T)56)*(i-1)*(i+2);
 		FOR(q,0,NN+1) {
-			T result = tr.sum(q);
+			T result = tr.sum(q-1);
 			if( result != A[q] ) {
 				success = false;
 				cerr << "Error in BIT::add/sum(" << q << "): Expected: " << A[q] << ", Actual: " << result << endl;
 			}
 		}
 		FOR(qL,0,N) FOR(qR,qL,qL+N) {
-			T result = tr.sum_range(qL,qR);
+			T result = tr.sum_range(qL,qR-1);
 			if( result != A[qR] - A[qL] ) {
 				success = false;
-				cerr << "Error in BIT::sum_range(" << qL << "," << qR << "): ";
+				cerr << "Error in BIT::sum_range(" << qL << "," << qR-1 << "): ";
 				cerr << "Expected: " << A[qR] - A[qL] << ", Actual: " << result << endl;
 			}
 		}
 	}
 	{
-		const size_t N = (1U << 7);
+		const int N = (1U << 7);
 		BIT tr(N);
 		VT A(N,0);
 		tr.add( 6, 14 );   A[6]   += 14;
@@ -206,10 +211,10 @@ void test_BIT_correct() {
 		VT AS(A.size()+1);
 		AS[0] = 0;
 		FOR(i,1,AS.size()) AS[i] = AS[i-1] + A[i-1];
-		size_t UB = AS.back();
+		int UB = AS.back();
 		FOR(q,0,UB) {
-			size_t result = tr.largest_at_most(q);
-			size_t exp = 0;
+			int result = tr.largest_at_most(q)+1;
+			int exp = 0;
 			while( exp+1 < AS.size() && AS[exp+1] <= (T)q ) ++exp;
 			if( result != exp ) {
 				success = false;
@@ -226,13 +231,13 @@ void test_BIT_correct() {
 void test_BIT_flip_correct() {
 	bool success = true;
 	{
-		const size_t N = (1U << 4);
+		const int N = (1U << 4);
 		BIT_flip tr(N);
 		VT A(N,0);
-		tr.add(3, 9, 16);  FOR(i,3,9)   A[i] += 16;
-		tr.add(1, 4, 2 );  FOR(i,1,4)   A[i] += 2;
-		tr.add(11,11,99);  FOR(i,11,11) A[i] += 99;
-		tr.add(0, 9, 12);  FOR(i,0,9)   A[i] += 12;
+		tr.add(3, 8, 16);  FOR(i,3,9)   A[i] += 16;
+		tr.add(1, 3, 2 );  FOR(i,1,4)   A[i] += 2;
+		tr.add(11,10,99);  FOR(i,11,11) A[i] += 99;
+		tr.add(0, 8, 12);  FOR(i,0,9)   A[i] += 12;
 		FOR(q,0,N) {
 			T result = tr.query( q );
 			T exp = A[q];
@@ -251,22 +256,22 @@ void test_BIT_flip_correct() {
 void test_BIT_super_correct() {
 	bool success = true;
 	{
-		const size_t N = (1U << 4);
+		const int N = (1U << 4);
 		BIT_super tr(N);
 		VT A(N,0);
 		FOR(i,0,N) FOR(j,i,N) {
 			T v = (27*i*(i+3)*(j+7)) % 93;
-			tr.add(i,j,v);
+			tr.add(i,j-1,v);
 			FOR(k,i,j) A[k] += v;
 		}
 		VT AS(A.size()+1);
 		AS[0] = 0; FOR(i,1,AS.size()) AS[i] = AS[i-1] + A[i-1];
 		FOR(qL,0,N) FOR(qR,qL,N) {
-			T result = tr.query_range(qL,qR);
+			T result = tr.query_range(qL,qR-1);
 			T exp = AS[qR] - AS[qL];
 			if( result != exp ) {
 				success = false;
-				cerr << "Error in BIT_super::add/query(" << qL << "," << qR << "): ";
+				cerr << "Error in BIT_super::add/query(" << qL << "," << qR-1 << "): ";
 				cerr << "Expected: " << exp << ", Actual: " << result << endl;
 			}
 		}
@@ -280,7 +285,7 @@ void test_BIT2_correct() {
 	typedef vector<VT> VVT;
 	bool success = true;
 	{
-		const size_t N = (1U << 5);
+		const int N = (1U << 5);
 		BIT2 tr(N,N);
 		VVT A(N,VT(N,0));
 		FOR(i,0,N) FOR(j,0,N) {
@@ -292,12 +297,12 @@ void test_BIT2_correct() {
 			T result = tr.sum_range(qi1,qj1, qi2,qj2);
 			T exp = 0;
 			// dumb ways are the best ways to test
-			FOR(ki,qi1,qi2) FOR(kj,qj1,qj2) exp += A[ki][kj];
+			FOR(ki,qi1,qi2+1) FOR(kj,qj1,qj2+1) exp += A[ki][kj];
 			if( result != exp ) {
 				success = false;
 				cerr << "Error in BIT2::add/query(" << qi1 << "," << qj1 << "  ->  " << qi2 << "," << qj2 << "): ";
 				cerr << "Expected: " << exp << ", Actual: " << result << endl;
-			}
+            }
 		}
 	}
 	if( success ) {
@@ -309,7 +314,7 @@ void test_BITd_correct() {
 	typedef vector<VT> VVT;
 	bool success = true;
 	{
-		const size_t N1 = (1U << 5), N2 = (1U << 4);
+		const int N1 = (1U << 5), N2 = (1U << 4);
 		VI dimn(2); dimn[0] = N1; dimn[1] = N2;
 		BITd tr( dimn, dimn.size() );
 		VVT A(N1,VT(N2,0));
@@ -326,12 +331,13 @@ void test_BITd_correct() {
 			T result = tr.sum_range(pos1,pos2);
 			T exp = 0;
 			// dumb ways are the best ways to test
-			FOR(ki,qi1,qi2) FOR(kj,qj1,qj2) exp += A[ki][kj];
+			FOR(ki,qi1,qi2+1) FOR(kj,qj1,qj2+1) exp += A[ki][kj];
 			if( result != exp ) {
 				success = false;
 				cerr << "Error in BITd::add/query(" << qi1 << "," << qj1 << "  ->  " << qi2 << "," << qj2 << "): ";
 				cerr << "Expected: " << exp << ", Actual: " << result << endl;
-			}
+                return;
+            }
 		}
 	}
 	if( success ) {
--- SegmentTree.cc
+++ SegmentTree.cc
@@ -4,163 +4,173 @@
 using namespace std;
 
 // BEGIN
+// Segment Tree with lazy propagation. This solves
+// AhoyPirates on UVa Online Judge in 1.172 seconds.
+// To use, you only need to change SegmentNode data, the merge function, update, and initialization;
+// the functions in the are written in such a way as to handle everything else.
+// If range updates are not necessary, ignore the updateVal and rangeUpdate function.
+// The following implementation is an example for RMQ.
 
-typedef signed long long int T;
+#define MAXN 1000 // the maximum input length of the sequnece, good idea to add 10 or so to this
+
+typedef signed long long int T; // the type of the underlying sequence
 typedef vector<size_t> VI;
 typedef vector<T> VT;
 
 struct SegmentNode {
-    size_t L, R;
-
     // segment data
     T maxVal;
 
     // update data
     T updateVal;
-
-    SegmentNode(size_t L, size_t R, T maxVal) : L(L), R(R), maxVal(maxVal) {
-        // initialize update values to no updates necessary
-        updateVal = 0;
-    }
-
-    SegmentNode() {}
 };
 
-struct SegmentTree {
-    vector<SegmentNode> st;
+SegmentNode st[MAXN*4];
+T A[MAXN];
 
-    size_t left(size_t cur) { return cur << 1; }
-    size_t right(size_t cur) { return (cur << 1) + 1; }
+size_t left(size_t cur) { return cur << 1; }
+size_t right(size_t cur) { return (cur << 1) + 1; }
 
-    // create merging function here
-    SegmentNode merge(SegmentNode &left, SegmentNode &right) {
-        return SegmentNode(left.L, right.R, max(left.maxVal, right.maxVal));
+// create merging function here
+void merge(SegmentNode &left, SegmentNode &right, SegmentNode &result) {
+    result.maxVal = max(left.maxVal, right.maxVal);
+}
+
+// only use if range update is needed
+void updateChildren(size_t cur, size_t L, size_t R);
+
+// merge handles all querying, no changes needed here
+// note that when calling any segment funcitons in main,
+// the first three parameters will be 1, 0, length-1
+SegmentNode query(size_t cur, size_t L, size_t R, size_t LQ, size_t RQ) {
+    if (L >= LQ && R <= RQ)
+        return st[cur];
+
+    updateChildren(cur, L, R);
+    size_t M = (L+R)/2;
+
+    if (M < LQ)
+        return query(right(cur), M+1, R, LQ, RQ);
+
+    if (M+1 > RQ)
+        return query(left(cur), L, M, LQ, RQ);
+
+    SegmentNode leftResult = query(left(cur), L, M, LQ, RQ);
+    SegmentNode rightResult = query(right(cur), M+1, R, LQ, RQ);
+    SegmentNode result;
+
+    merge(leftResult, rightResult, result);
+
+    return result;
+}
+
+// only implement if necessary
+void update(size_t cur, size_t L, size_t R, size_t idx, T val) {
+    if (L == idx && R == idx) {
+        // write update of single value here
+        st[cur].maxVal = val;
     }
+    else if (L <= idx && R >= idx) {
+        size_t M = (L+R)/2;
+        update(left(cur), L, M, idx, val);
+        update(right(cur), M+1, R, idx, val);
 
-    // merge handles all querying, no changes needed here
-    SegmentNode query(size_t cur, size_t LQ, size_t RQ) {
-        if (st[cur].L >= LQ && st[cur].R <= RQ)
-            return st[cur];
-
-        updateChildren(cur);
-
-        if (st[left(cur)].R < LQ)
-            return query(right(cur), LQ, RQ);
-
-        if (st[right(cur)].L > RQ)
-            return query(left(cur), LQ, RQ);
-
-        SegmentNode leftResult = query(left(cur), LQ, RQ);
-        SegmentNode rightResult = query(right(cur), LQ, RQ);
-
-        return merge(leftResult, rightResult);
+        merge(st[left(cur)], st[right(cur)], st[cur]);
     }
+}
 
-    // do not use if rangeUpdate is needed
-    void update(size_t cur, size_t idx, T val) {
-        if (st[cur].L == idx && st[cur].R == idx) {
-            // write update of single value here
-            st[cur].maxVal = val;
-        }
-        else if (st[cur].L <= idx && st[cur].R >= idx) {
-            update(left(cur), idx, val);
-            update(right(cur), idx, val);
+// only implement if necessary
+void rangeUpdate(size_t cur, size_t L, size_t R, size_t Lbound, size_t Rbound, T val) {
+    if (L >= Lbound && R <= Rbound) {
+        // implement range update here
+        st[cur].maxVal += val;
 
-            st[cur] = merge(st[left(cur)], st[right(cur)]);
-        }
+        // set update vals here
+        st[cur].updateVal += val;
     }
+    else if (L <= Rbound && R >= Lbound) {
+        updateChildren(cur, L, R);
 
-    // only implement if necessary
-    void rangeUpdate(size_t cur, size_t Lbound, size_t Rbound, T val) {
-        if (st[cur].L >= Lbound && st[cur].R <= Rbound) {
-            // implement range update here
-            st[cur].maxVal += val;
+        size_t M = (L+R)/2;
+        rangeUpdate(left(cur), L, M, Lbound, Rbound, val);
+        rangeUpdate(right(cur), M+1, R, Lbound, Rbound, val);
 
-            // set update vals here
-            st[cur].updateVal += val;
-        }
-        else if (st[cur].L <= Rbound && st[cur].R >= Lbound) {
-            updateChildren(cur);
-
-            rangeUpdate(left(cur), Lbound, Rbound, val);
-            rangeUpdate(right(cur), Lbound, Rbound, val);
-
-            st[cur] = merge(st[left(cur)], st[right(cur)]);
-        }
+        merge(st[left(cur)], st[right(cur)], st[cur]);
     }
+}
 
-    void updateChildren(size_t cur) {
-        rangeUpdate(left(cur), st[cur].L, st[cur].R, st[cur].updateVal);
-        rangeUpdate(right(cur), st[cur].L, st[cur].R, st[cur].updateVal);
+void updateChildren(size_t cur, size_t L, size_t R) {
+    rangeUpdate(left(cur), L, R, L, R, st[cur].updateVal);
+    rangeUpdate(right(cur), L, R, L, R, st[cur].updateVal);
 
-        // reset update vals
-        st[cur].updateVal = 0;
+    // reset update vals
+    st[cur].updateVal = 0;
+}
+
+void build(size_t cur, size_t L, size_t R) {
+    // initialize update vals
+    st[cur].updateVal = 0;
+    
+    if (L == R) {
+        // initialize single valiue here
+        st[cur].maxVal = A[L];
     }
+    else {
+        size_t M = (L+R)/2;
+        build(left(cur), L, M);
+        build(right(cur), M+1, R);
 
-    void build(size_t cur, size_t L, size_t R, VT &A) {
-        if (L == R) {
-            // initialize single value here
-            st[cur] = SegmentNode(L, R, A[L]);
-        }
-        else {
-            build(left(cur), L, (L+R)/2, A);
-            build(right(cur), (L+R)/2+1, R, A);
-
-            st[cur] = merge(st[left(cur)], st[right(cur)]);
-        }
+        merge(st[left(cur)], st[right(cur)], st[cur]);
     }
+}
 
-    SegmentTree(VT &A) {
-        st = vector<SegmentNode>(A.size()*4);
-        build(1, 0, A.size()-1, A);
-    }
-};
 // END
 
 void test_case(T expected, T actual) {
     cerr << "Expected: " << expected << " Actual: " << actual << endl;
 }
 
-void test_segment_tree_correct() {
+void test_segment_correct() {
     size_t N = 10;
-    T vals[] = {0, 42, 3, 4, 5, 983, 48, 28, 59, 1};
-
-    VT A(vals, vals+N);
-
-    SegmentTree tree(A);
+    int cur[] = {0, 42, 3, 4, 5, 983, 48, 28, 59, 1};
+    for (size_t i = 0; i < N; ++i) {
+        A[i] = cur[i];
+    }
+    
+    build(1, 0, N-1);
 
     bool testLazy = true;
 
-    cerr << "test segment tree correctness" << endl;
+    cerr << "test segment correctness" << endl;
     if (!testLazy) {
-        test_case(tree.query(1, 0, 3).maxVal, 42);
-        test_case(tree.query(1, 2, 4).maxVal, 5);
+        test_case(query(1, 0, N-1, 0, 3).maxVal, 42);
+        test_case(query(1, 0, N-1, 2, 4).maxVal, 5);
         
-        tree.update(1, 1, -10); //42 -> -10
-        test_case(tree.query(1, 0, 3).maxVal, 4);
-        test_case(tree.query(1, 0, 9).maxVal, 983);
+        update(1, 0, N-1, 1, -10); //42 -> -10
+        test_case(query(1, 0, N-1, 0, 3).maxVal, 4);
+        test_case(query(1, 0, N-1, 0, 9).maxVal, 983);
         
-        tree.update(1, 5, 80); //983 -> 80
-        test_case(tree.query(1, 0, 9).maxVal, 80);
-        test_case(tree.query(1, 6, 8).maxVal, 59);
+        update(1, 0, N-1, 5, 80); //983 -> 80
+        test_case(query(1, 0, N-1, 0, 9).maxVal, 80);
+        test_case(query(1, 0, N-1, 6, 8).maxVal, 59);
     }
     else {
-        test_case(tree.query(1, 0, 3).maxVal, 42);
-        test_case(tree.query(1, 2, 4).maxVal, 5);
+        test_case(query(1, 0, N-1, 0, 3).maxVal, 42);
+        test_case(query(1, 0, N-1, 2, 4).maxVal, 5);
 
-        tree.rangeUpdate(1, 2, 4, 40); //3, 4, 5 -> 43, 44, 45
-        test_case(tree.query(1, 0, 3).maxVal, 44);
-        test_case(tree.query(1, 0, 9).maxVal, 983);
+        rangeUpdate(1, 0, N-1, 2, 4, 40); //3, 4, 5 -> 43, 44, 45
+        test_case(query(1, 0, N-1, 0, 3).maxVal, 44);
+        test_case(query(1, 0, N-1, 0, 9).maxVal, 983);
 
-        tree.rangeUpdate(1, 0, 9, 10); //everything +10
-        test_case(tree.query(1, 0, 9).maxVal, 993);
-        test_case(tree.query(1, 6, 8).maxVal, 69);
+        rangeUpdate(1, 0, N-1, 0, 9, 10); //everything +10
+        test_case(query(1, 0, N-1, 0, 9).maxVal, 993);
+        test_case(query(1, 0, N-1, 6, 8).maxVal, 69);
     }
 
 }
 
 int main() {
-    test_segment_tree_correct();
+    test_segment_correct();
 
     return 0;
 }
