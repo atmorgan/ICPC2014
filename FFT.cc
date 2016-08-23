df57b319ea8e3a19621217ef643761248c2f03be
Bryce Sandlund <bcsandlund@gmail.com>
2016-08-23 18:11:53 -0500

    Updated FFT to version about 8 times faster

--- FFT.cc
+++ FFT.cc
@@ -1,25 +1,16 @@
 #include <complex>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-#define FOR(v,l,u) for( size_t v = l; v < u; ++v )
-
-// Implementation notes:
-//
-// - FFT_r uses arrays (as opposed to vectors), since that lets us do pointer
-// arithmetic without needing to think about iterators. This makes the code
-// quite simple overall.
-//
-// - FFT is just a wrapper that lets us think in vectors even though the FFT
-// is implemented with arrays.
-
 // BEGIN
-// Based heavily off the implementation found at
-// http://web.stanford.edu/~liszt90/acm/notebook.html#file16
+// Based on implementation at: http://e-maxx.ru/algo/fft_multiply,
+// first seen from: http://serjudging.vanb.org/wp-content/uploads/kinversions_hcheng.cc
+// Solves UVa Online Judge "Golf Bot" in 2.35 seconds
 
 // Usage:
-// f[0...N-1] and g[0..N-1] are numbers
+// f[0...N-1] and g[0..N-1] are numbers, N is a power of 2.
 // Want to compute the convolution h, defined by
 // h[n] = sum of f[k]g[n-k] (k = 0, ..., N-1).
 // Here, the index is cyclic; f[-1] = f[N-1], f[-2] = f[N-2], etc.
@@ -27,64 +18,64 @@ using namespace std;
 // The convolution theorem says H[n] = F[n]G[n] (element-wise product).
 // To compute h[] in O(N log N) time, do the following:
 //   1. Compute F and G (pass d = 1 as the argument).
-//   2. Get H by element-wise multiplying F and G.
+//   2. Get H by element-wise multiplying F and G
 //   3. Get h by taking the inverse FFT (pass d = -1)
 
-// Example situation:
-// You have polynomials p(x) and q(x), stored as the coefficients of
-// the powers of x, and you want to compute (pq)(x) in the same format.
-// This is the same as the convolution of the coefficient vectors.
-// Let N be so that N is a power of 2 and N/2 >= max(deg(p),deg(q)).
-// Zero-pad the coefficients of p and q to have size N.
-// Now convolve p and q: compute FFT(p) and FFT(q). Multiply component-wise.
-// compute FFT^{-1}( result ). This is pq.
-//
-// In code:
-// FFT(p,1); FFT(q,1); FOR(i,0,N) pq[i] = p[i]q[i]; FFT(pq,-1);
+#define MAX_N (1 << 3)     // must be power of 2
 
 typedef complex<double> T;
 typedef vector<T> VT;
-
 const double PI = 4*atan(1);
 
-void FFT_r( T *A, T *B, size_t p, size_t n, int d ) {
-	if( n == 1 ) { B[0] = A[0]; return; }
-	FFT_r( A  , B    , 2*p, n/2, d );
-	FFT_r( A+p, B+n/2, 2*p, n/2, d );
-	FOR(k,0,n/2) {
-		T w = polar(1.0, 2*PI*k/n*d);
-		T even = B[k], odd = B[k+n/2];
-		B[k    ] = even + w * odd;
-		B[k+n/2] = even - w * odd;
-	}
-}
-void FFT( VT &_A, int d ) {
-	const size_t n = _A.size();
-	T *A = new T[n], *B = new T[n];
-	FOR(i,0,n) A[i] = _A[i];
-	FFT_r( A, B, 1, n, d );
-	FOR(i,0,n) _A[i] = B[i];
-	delete[] A;
-	delete[] B;
-	if( d < 0 ) FOR(i,0,n) _A[i] /= n;
+void FFT(VT &a, bool invert) {
+    size_t n = a.size();
+
+    for (int i=1, j=0; i<n; ++i) {
+        int bit = n >> 1;
+        for (; j>=bit; bit>>=1)
+            j -= bit;
+        j += bit;
+        if (i < j)
+            swap(a[i], a[j]);
+    }
+
+    for (int len=2; len<=n; len<<=1) {
+        double ang = 2*PI/len * (invert ? -1 : 1);
+        T wlen (cos(ang), sin(ang));
+        for (int i=0; i<n; i+=len) {
+            T w(1);
+            for (int j=0; j<len/2; ++j) {
+                T u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+
+    if(invert)
+        for (int i=0; i<n; ++i)
+            a[i] /= n;
 }
 // END
 
 #include <iostream>
 #include <iomanip>
+#define FOR(v,l,u) for( size_t v = l; v < u; ++v )
 
 int main(void)
 {
   cout << "If rows come in identical pairs, then everything works." << endl;
   cout << setprecision(8) << fixed;
-  
-  const size_t n = 8;
+  cout << "Test FFT:" << endl;
+
+  const size_t n = MAX_N;
   T _a[n] = {0, 1, T(1,3), T(0,5), 1, 0, 2, 0};
   T _b[n] = {1, T(0,-2), T(0,1), 3, -1, -3, 1, -2};
   VT a(_a, _a+n), b(_b,_b+n);
   VT A = a, B = b;
-  FFT(A,1);
-  FFT(B,1);
+  FFT(A,false);
+  FFT(B,false);
   
   // Test convolution
   cout << A[0]; FOR(i,1,n) cout << " " << A[i]; cout << endl;
@@ -100,7 +91,7 @@ int main(void)
   VT AB(n);
   FOR(i,0,n) AB[i] = A[i] * B[i];
   VT ab(AB);
-  FFT(ab,-1);
+  FFT(ab,true);
   cout << ab[0]; FOR(i,1,n) cout << " " << ab[i]; cout << endl;
   FOR(i,0,n) {
     T aconvbi = 0;
@@ -109,6 +100,6 @@ int main(void)
     cout << aconvbi;
   }
   cout << endl;
-  
+
   return 0;
 }
